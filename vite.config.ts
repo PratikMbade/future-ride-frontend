1f850c07c4fc692217ca701a34e600e421b77c7a
PratikMbade <pratikmbade18@gmail.com>
2026-06-25 09:51:24 +0000

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: 'node-server' }),
+    viteReact(),
+  ],
 })
-
 export default config
